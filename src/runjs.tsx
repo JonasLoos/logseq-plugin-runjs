4c19163f228f63d74a8079fddef515807cae2c6e
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2023-12-07 16:09:47 +0100

    try new method for replaceBlock

--- src/runjs.tsx
+++ src/runjs.tsx
@@ -1,8 +1,23 @@
 import "@logseq/libs";
 import React from "react";
 
-export default function (props: { content: string, uuid: string }) {
-  const { content, uuid } = props;
+async function getBlockFromContent(content: string): any {
+  // go though all blocks on the page until a block with the given content is found
+  const blocks = [];
+  const tree = await logseq.Editor.getCurrentPageBlocksTree();
+  blocks.push(...tree);
+  while (blocks.length) {
+    const block = blocks.pop();
+    if (block) {
+      if (block.content == content) return block;
+      blocks.push(...block.children);
+    }
+  }
+  throw new Error('Block not found');
+}
+
+export default function (props: { content: string }) {
+  const { content } = props;
   const elRef = React.useRef(null);
   const [evalOutput, setEvalOutput] = React.useState("");
 
@@ -12,13 +27,19 @@ export default function (props: { content: string, uuid: string }) {
       // TODO(meain): allow to do more than just text
       elRef.current.textContent = text;
     };
-    const replaceBlock = (text) => {
-      logseq.Editor.updateBlock(uuid, text)
-    }
+    const replaceBlock = async (text) => {
+      const uuid = await getBlockFromContent(content).uuid;
+      logseq.Editor.updateBlock(uuid, text);
+    };
 
     setTimeout(() => {
       // Have to put this in a timeout or LogSeq freaks out
-      eval(content);
+      try {
+        eval(content);
+      } catch (error) {
+        console.error('Error in eval:', error);
+        setOutput('Error in execution: ' + error);
+      }
     }, 100);
   }, []);
 
